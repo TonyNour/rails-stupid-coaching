ecbe9d8e2de9cba9db19c6bc824872596a924d14
Tony Nour <tonyanour1@gmail.com>
2024-04-29 16:13:07 +0100

    as

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  # verb "url", to: "controller#action"
+  def ask
+  end
+
+  def answer
+    @answers = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = @answers[0]
+    elsif @question[-1] == '?'
+      @answer = @answers[1]
+    else
+      @answer = @answers[2]
+    end
+  end
+end
